10ae926b6132f0cbd664e466b90a0265aa3ac84c
FIC <FIC@info.local>
2020-09-26 17:29:29 -0300

    primeiro commit do curso

---
+++ Pessoa/src/curso/Curso.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package curso;
+
+/**
+ *
+ * @author fic
+ */
+public class Curso {
+    
+    String nome;
+    int qtdealunos;
+    String turma;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQtdealunos() {
+        return qtdealunos;
+    }
+
+    public void setQtdealunos(int qtdealunos) {
+        this.qtdealunos = qtdealunos;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+    
+}
---
+++ Pessoa/src/triangulo/Triangulo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package triangulo;
+
+/**
+ *
+ * @author fic
+ */
+public class Triangulo {
+    
+    float base;
+    float altura;
+
+    public float getBase() {
+        return base;
+    }
+
+    public void setBase(float base) {
+        this.base = base;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }    
+}
